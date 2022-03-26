18f70f64ec3f0ac908312d41c818d703506601ba
Eyyub Oruczade <oruczade0491@gmail.com>
2022-03-26 19:00:38 +0400

    second page

--- app/src/main/java/com/example/yttask/MainActivity.kt
+++ app/src/main/java/com/example/yttask/MainActivity.kt
@@ -17,6 +17,7 @@ import android.widget.AdapterView.OnItemSelectedListener as OnItemSelectedListen
 
 
 class MainActivity : AppCompatActivity() {
+
     private var txt: TextView? = null
     private var spin: Spinner? = null
     private var image: ImageView? = null
@@ -24,30 +25,46 @@ class MainActivity : AppCompatActivity() {
     private var mButton: Button? = null
     private var pricetext: TextView? = null
     private var nametext: EditText? = null
-    private var buttonAdd:Button?=null
+    private var buttonAdd: Button? = null
+    private var buttonOrder: Button? = null
+    private var posterImage: ImageView? = null
     private var instruments = HashMap<String, Int>()
+    private val goodsList = arrayListOf<Goods>()
+    private var clearButton : Button?=null
 
+    var goodsName: String = ""
     var a = 0
+    var order = Order()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        buttonAdd=findViewById(R.id.button5)
-        instruments["Guitar"] = 1000
-        instruments["Drum"] = 500
-        instruments["Piano"] = 1500
-        image = findViewById(R.id.goodsImage)
-        pricetext = findViewById(R.id.priceValue)
-        var price: Int
-        var goodsName: String
-        goodsName = spin?.selectedItem.toString()
-        val mutab = arrayOf("Guitar", "Drum", "Piano")
+
+        buttonOrder = findViewById(R.id.button5)
+        posterImage = findViewById(R.id.imageView)
+        buttonAdd = findViewById(R.id.button2)
         mButton = findViewById(R.id.minusButton)
         pButton = findViewById(R.id.plusButton)
         spin = findViewById(R.id.spinner)
+        image = findViewById(R.id.goodsImage)
+        pricetext = findViewById(R.id.priceValue)
+        nametext = findViewById(R.id.nametxt)
+        txt = findViewById(R.id.quantityValue)
+        clearButton=findViewById(R.id.clear)
+
+        instruments["Guitar"] = 1000
+        instruments["Drum"] = 500
+        instruments["Piano"] = 1500
+
+        val mutable1 = arrayOf("Guitar", "Drum", "Piano")
         var p = 0
-        val massiv = ArrayAdapter(this, android.R.layout.simple_spinner_item, mutab)
-        massiv.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spin?.adapter = massiv
+        val array1 = ArrayAdapter(this, android.R.layout.simple_spinner_item, mutable1)
+
+        goodsName = spin?.selectedItem.toString()
+
+        array1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spin?.adapter = array1
+
         spin?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -60,6 +77,7 @@ class MainActivity : AppCompatActivity() {
                     "Drum" -> {
                         a = 0
                         p = 0
+
                         image?.setImageResource(R.drawable.drum)
                         pricetext?.text = (0).toString()
 
@@ -68,7 +86,6 @@ class MainActivity : AppCompatActivity() {
                             p += 500
                             pricetext?.text = (p).toString()
                             a += 1
-                            txt = findViewById(R.id.quantityValue)
                             txt?.text = (a).toString()
                         }
                         mButton?.setOnClickListener() {
@@ -93,7 +110,6 @@ class MainActivity : AppCompatActivity() {
                             p += 1000
                             pricetext?.text = (p).toString()
                             a += 1
-                            txt = findViewById(R.id.quantityValue)
                             txt?.text = (a).toString()
                         }
                         mButton?.setOnClickListener() {
@@ -117,7 +133,6 @@ class MainActivity : AppCompatActivity() {
                             p += 1500
                             pricetext?.text = (p).toString()
                             a += 1
-                            txt = findViewById(R.id.quantityValue)
                             txt?.text = (a).toString()
                         }
                         mButton?.setOnClickListener() {
@@ -138,24 +153,54 @@ class MainActivity : AppCompatActivity() {
                 TODO("Nothing implemented yet")
             }
         }
-        buttonAdd?.setOnClickListener(){
-            var order = Order()
-            nametext = findViewById(R.id.nametxt)
-            price = instruments.get(goodsName)!!
+
+        clearButton?.setOnClickListener(){
+            goodsList[0].count=0
+            goodsList[1].count=0
+            goodsList[2].count=0
+        }
+
+        buttonOrder?.setOnClickListener() {
             order.userName = (nametext?.getText()).toString()
             order.goods = goodsName
             order.quantity = a
-            order.priceValue = price * a
-            var intent=Intent(this,OrderActivity::class.java).also {
+
+            var intent = Intent(this, OrderActivity::class.java).also {
                 it.putExtra("userName", order.userName)
-                it.putExtra("goodsName", order.goods)
-                it.putExtra("quantity", order.quantity)
-                it.putExtra("price", order.priceValue)
-                it.putExtra("itemPrice",price)
+                it.putExtra("GuitarCount", goodsList[0].count)
+                it.putExtra("DrumCount", goodsList[1].count)
+                it.putExtra("PianoCount", goodsList[2].count)
                 startActivity(it)
             }
         }
 
+        setGoods()
+        setOnClick()
+    }
+
+    private fun setGoods() {
+        val guitar = Goods("Guitar", 0, 1000)
+        val drum = Goods("Drum", 0, 500)
+        val piano = Goods("Piano", 0, 1500)
+        goodsList.add(guitar) //0
+        goodsList.add(drum)  //1
+        goodsList.add(piano)  //2
+    }
+
+    private fun setOnClick() {
+        buttonAdd?.setOnClickListener() {
+            when (goodsName) {
+                "Drum" -> {
+                    goodsList[1].count = txt?.text.toString().toIntOrNull() ?: 0
+                }
+                "Guitar" -> {
+                    goodsList[0].count = txt?.text.toString().toIntOrNull() ?: 0
+                }
+                "Piano" -> {
+                    goodsList[2].count = txt?.text.toString().toIntOrNull() ?: 0
+                }
+            }
+        }
 
     }
 }
--- app/src/main/java/com/example/yttask/Order.kt
+++ app/src/main/java/com/example/yttask/Order.kt
@@ -1,10 +1,15 @@
 package com.example.yttask
 
+import android.widget.Button
+
 class Order {
 
     var userName: String? =null
     var goods:String?=null
     var quantity:Int?=null
     var priceValue:Int?=null
+    var secondPriceValue:String?=null
+
+
 
 }
